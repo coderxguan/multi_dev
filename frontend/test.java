a822eec6a6cdccf0d2ff40d156ecf471f6a5095d
dghyuiok <14341407+dghyuiok@user.noreply.gitee.com>
2025-03-31 15:21:42 +0800

    第二次提交代码和类

--- frontend/test.java
+++ frontend/test.java
@@ -1,3 +1,5 @@
-public static void main(String[] args) {
-    System.out.println("这是我上传的代码和类");
+public class test {
+    public static void main(String[] args) {
+        System.out.println("这是第二次提交的代码和类");
+    }
 }
